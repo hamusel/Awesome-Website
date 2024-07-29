563400d708c2c2edbcec19fe7417d2207601516e
Alexandru Iulian Gheorghita <gheorghita@cip.ifi.lmu.de>
2024-07-29 19:48:01 +0200

    New Website

---
+++ cdc.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+//int main() {
+//    int x;
+//    cin >> x;
+//    while (x > 9) {
+//        int sum = 0;
+//        while (x) {
+//            sum += x % 10;
+//            x /= 10;
+//        }
+//        x = sum;
+//    }
+//    cout << x;
+//    return 0;
+//}
